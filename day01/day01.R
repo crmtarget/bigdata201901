653c6c5aece621c9a4343489fa6e686f727e9b09
crmtarget <47075037+crmtarget@users.noreply.github.com>
2019-01-27 17:54:46 +0900

    Add files via upload

--- day01/day01.R
+++ day01/day01.R
@@ -51,3 +51,52 @@ name[3]
 print(name)
 name <- c("CRM","HE","냉장고","RAC","세탁기"
 print(name)
+
+# factor 구분하기
+adderss <- c("서울", "대구", "부산", "군산", "서울", "서울", "대구")
+address <- c("서울", "대구", "부산", "군산", "서울", "서울", "대구")
+
+chr[1:7] "서울", "대구", "부산", "군산", "서울", "서울", "대구"
+str(address)
+address2 <- as.factor(address)
+str(address2)
+
+address2[1] <- "대구"
+address <- c("강남", "강북", "경인", "중부", "중원", "서부", "경북", "경남")
+
+address[2]
+address2 <- as.factor(address)
+str(address2)
+
+숫자를　입력했을　때
+기본　값은　무조건　스트링（문자열）
+＝＞　숫자로　변환　기본적으로　모든　입력　출력은　스트링
+
+# 입력의 다양한 형태
+X <- scan()
+x<- scan()
+
+str(x2)
+class(x2)
+typeof(x2)
+
+food <- scan(what = "")
+"피자"
+"냉장고"
+"세탁기"
+
+str(food)
+class(food)
+typeof(food)
+
+favorite <- food[2]
+print("내가 제일 좋아하는 음식은", favorite )
+str(food)
+cat("내가 제일 좋아하는 음식은", favorite )
+
+library(glue)
+
+favorite <- food[1]
+cat("내가 제일 좋아하는 음식은",favorite)
+second <- food[2]
+glue("나의 {second}음식은 2번째로 좋아하는 음식이에요,")
