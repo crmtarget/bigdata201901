d263eb73c35d052d7b230482eda157af61caa0fa
crmtarget <47075037+crmtarget@users.noreply.github.com>
2019-01-27 16:29:55 +0900

    Add files via upload

---
+++ day01/day01.R
@@ -0,0 +1,53 @@
+
+//외부 라이브러리 설ㅊ
+install.packages("glue")
+install.packages("ggplot2")
+
+# //외부 라이브러리 사용자 지칭
+library(glue)
+library(glue2)
+
+# 여기에 glue를 이용해서 프린트 연습
+install.packages("glue")
+he = man
+
+  temp 변수 : 아무 것도 안붙어있다, 정적인 값이다.상대적으로 정적인 값임
+              정적인 값을 저장할 때 사용
+  temp() :  함수 : 동적인 처리
+  
+ # 벡터 만들어보기
+  age <-  (1,20,30)
+  
+# 할당연산자 <-  알트(-)
+  
+  평균  %>%  정렬 %>%  프린트
+
+# 파이프 연산자 : %>% : 계속해서 연산하자(알트+/)
+
+  평균  %>%  정렬 %>%  프린트
+  
+  ages <- c(10, 30, 50)
+  
+  ages[1] <-
+    
+# 이름벡터를　만들고
+# 두번째　사람이　박아무개로　개명
+# 이름의　전체　내용　출력
+    
+name <- c("김아무개", "박아무개", "최아무개")
+name[2] <-"최진혁"
+print(name)
+print(name[2])  
+
+name <- c("CRM","HE","냉장고","RAC","세탁기")
+print(name[2])
+print(name[2])
+name[1] <- "CRM파트장"
+print(name[1])
+print(name[2])  
+name[2] <- '김병주팀장'
+print(name[2])
+name[3]
+print(name)
+name <- c("CRM","HE","냉장고","RAC","세탁기"
+print(name)
